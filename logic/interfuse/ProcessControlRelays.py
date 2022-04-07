f8a58db0db54e4f0d052650286ee9509c5b9df30
Elias Sfeir <elias.sfeir@etu.umontpellier.fr>
2022-04-07 15:44:16 +0200

    Final version for interfuse power supply an arduino

--- logic/interfuse/ProcessControlRelays.py
+++ logic/interfuse/ProcessControlRelays.py
@@ -24,22 +24,21 @@ from core.connector import Connector
 from logic.generic_logic import GenericLogic
 from interface.process_control_interface import ProcessControlInterface
 import time
+import numpy as np 
 
 class ProcessControlRelays(GenericLogic, ProcessControlInterface):
     """ This interfuse can be used to combin the work of Powersuplly and the Arduino.
     How check the  value of the polarity and switch the coil 
     """
     
-    _powerSupply = Connector(interface='ProcessControlInterface')
-    _arduino_Relays = Connector(interface = 'Interface_control_arduino')
+    powersupply = Connector(interface='ProcessControlInterface')
+    arduinorelays = Connector(interface = 'Interface_control_arduino')
     
     
 
     def on_activate(self):
         """ Activate module.
         """
-        self._powerSupply()
-        self._arduino_Relays()
 
         
 
@@ -49,60 +48,6 @@ class ProcessControlRelays(GenericLogic, ProcessControlInterface):
         return   
         
 # interface functions (arduino )   
-    def init_all_pins(self):
-        """ initializes all pin in output mode """ 
-        
-        
-        self.Arduino_Relays.init_all_pins()
-        return
-
-    def set_pin_mode(self, pin_number, mode):
-         """
-        Performs a pinMode() operation on pin_number
-        Internally sends b'M{mode}{pin_number} where mode could be:
-        - I for INPUT
-        - O for OUTPUT
-        - P for INPUT_PULLUP MO13
-        """
-        
-         self.Arduino_Relays.set_pin_mode(self, pin_number, mode)
-         return
-     
-    def digital_read(self, pin_number):   
-        """
-        Performs a digital read on pin_number and returns the value (1 or 0)
-        Internally sends b'RD{pin_number}' over the serial connection
-        """
-        
-        self.Arduino_Relays.digital_read(self, pin_number)
-        return
-    
-    def digital_write(self, pin_number, digital_value):
-         """
-        Writes the digital_value on pin_number
-        Internally sends b'WD{pin_number}:{digital_value}' over the serial
-        connection
-        """
-         self.Arduino_Relays.digital_write(self, pin_number,digital_value)
-         return 
-        
-    def analog_read(self, pin_number):
-         """
-        Performs an analog read on pin_number and returns the value (0 to 1023)
-        Internally sends b'RA{pin_number}' over the serial connection
-        """
-         self.Arduino_Relays.analog_read(self, pin_number)
-         return 
-    
-    
-    def analog_write(self, pin_number, analog_value):
-        """
-        Writes the analog value (0 to 255) on pin_number
-        Internally sends b'WA{pin_number}:{analog_value}' over the serial
-        connection
-        """   
-        self.Arduino_Relays.analog_write(self, pin_number,analog_value)
-        return 
 
     def switch_coil(self, polarity,coil):
        """
@@ -111,31 +56,31 @@ class ProcessControlRelays(GenericLogic, ProcessControlInterface):
         """
        if coil == 1 :  # x coil
            if polarity == 'neg':
-               self.digital_write(self._pin_list[0],1)
+               self.arduinorelays().digital_write(self._pin_list[0],1)
                time.sleep(0.5)
-               self.digital_write(self._pin_list[1],1)
+               self.arduinorelays().digital_write(self._pin_list[1],1)
            else:
-              self.digital_write(self._pin_list[0],0)
+              self.arduinorelays().digital_write(self._pin_list[0],0)
               time.sleep(0.5)
-              self.digital_write(self._pin_list[1],0)
+              self.arduinorelays().digital_write(self._pin_list[1],0)
        elif coil == 2     :  # y coil 
            if polarity == 'neg':
-              self.digital_write(self._pin_list[2],0)
+              self.arduinorelays().digital_write(self._pin_list[2],0)
               time.sleep(0.5)
-              self.digital_write(self._pin_list[3],0)
+              self.arduinorelays().digital_write(self._pin_list[3],0)
            else:
-               self.digital_write(self._pin_list[2],1)
+               self.arduinorelays().digital_write(self._pin_list[2],1)
                time.sleep(0.5)
-               self.digital_write(self._pin_list[3],1)
+               self.arduinorelays.digital_write(self._pin_list[3],1)
        elif coil == 3  :  #  z coil 
             if polarity == 'neg':
-                self.digital_write(self._pin_list[4],1)
+                self.arduinorelays.digital_write(self._pin_list[4],1)
                 time.sleep(0.5)
-                self.digital_write(self._pin_list[5],1)
+                self.arduinorelays.digital_write(self._pin_list[5],1)
             else:
-                self.digital_write(self._pin_list[4],0)
+                self.arduinorelays.digital_write(self._pin_list[4],0)
                 time.sleep(0.5)
-                self.digital_write(self._pin_list[5],0)
+                self.arduinorelays.digital_write(self._pin_list[5],0)
        return
 
 
@@ -154,7 +99,7 @@ class ProcessControlRelays(GenericLogic, ProcessControlInterface):
         else :
             self.switch_coil('pos',channel)
         time.sleep(5)   
-        self.PowerSupply.set_control_value(self, value, channel=1, ctrparam="VOLT")
+        self.PowerSupply().set_control_value( np.abs(value), channel=1, ctrparam="VOLT")
         pass
 
    
@@ -165,7 +110,7 @@ class ProcessControlRelays(GenericLogic, ProcessControlInterface):
 
         @return (float): The current control value
         """
-        self.PowerSupply.get_control_value(self, channel=None, ctrparam="VOLT")
+        self.PowerSupply().get_control_value(channel=None, ctrparam="VOLT")
         pass
 
     
@@ -176,7 +121,7 @@ class ProcessControlRelays(GenericLogic, ProcessControlInterface):
 
         @return: The unit as a tuple of ('abbreviation', 'full unit name')
         """
-        self.PowerSupply.get_control_unit(self, ctrparam="VOLT")
+        self.PowerSupply().get_control_unit( ctrparam="VOLT")
         pass
 
     
@@ -187,7 +132,7 @@ class ProcessControlRelays(GenericLogic, ProcessControlInterface):
 
         @return (tuple): The limits as (low limit, high limit)
         """
-        self.PowerSupply.get_control_limit(self, channel=None, ctrparam="VOLT")
+        self.PowerSupply().get_control_limit( channel=None, ctrparam="VOLT")
         pass
 
     def process_control_supports_multiple_channels(self):
@@ -197,7 +142,7 @@ class ProcessControlRelays(GenericLogic, ProcessControlInterface):
 
         This function is not abstract - Thus it is optional and if a hardware do not implement it, the answer is False.
         """
-        self.PowerSupply.process_control_supports_multiple_channels()
+        self.PowerSupply().process_control_supports_multiple_channels()
         return False
 
     def process_control_get_number_channels(self):
@@ -207,7 +152,7 @@ class ProcessControlRelays(GenericLogic, ProcessControlInterface):
 
         This function is not abstract - Thus it is optional and if a hardware do not implement it, the answer is 1.
         """
-        self.PowerSupply.process_control_get_number_channels()
+        self.PowerSupply().process_control_get_number_channels()
         return 1
     
     
