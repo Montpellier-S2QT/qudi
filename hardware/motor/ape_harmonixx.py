204e1cc173ba76c00667c205229c3f915e83a962
Adrien Rousseau <adrienrousseau9@gmail.com>
2022-05-27 08:59:59 +0200

    APE harmonixx using TCP (not working ?!)

--- hardware/motor/ape_harmonixx.py
+++ hardware/motor/ape_harmonixx.py
@@ -18,13 +18,14 @@ along with Qudi. If not, see <http://www.gnu.org/licenses/>.
 Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 """
+import time
 from collections import OrderedDict
 
 from core.module import Base
 from core.configoption import ConfigOption
 from interface.motor_interface import MotorInterface
 
-import visa
+import socket
 
 class Harmonixx(Base, MotorInterface):
 
@@ -37,48 +38,113 @@ class Harmonixx(Base, MotorInterface):
 
     ape_harmonixx:
         module.Class: 'motor.ape_harmonixx.Harmonixx'
-        port: 'COM5'
+        host: '127.0.0.1'
+        port: '51300'
 
     """
 
+    _host = ConfigOption('host', default="127.0.0.1")
     _port = ConfigOption('port', missing='error')
 
     def on_activate(self):
         """ Initialisation performed during activation of the module.
         """
-        self._rm = visa.ResourceManager()
 
-        self._device = self._rm.open_resource(self._port)
-        self._device.baud_rate = 921600
-        self._device.read_termination = "\r\n"
+        self.dev = None
+        self.connected = False
 
-        self._axis = ["SHG", "WP", "DC", "THG", "WAVELENGTH"]
+        if not isinstance(self._port, int) or not self._port or not 1 <= self._port <= 65535:
+            self.log.error('Portnumber must be passed as integer (range 1..65535)')
+        else:
+            try:
+                self.dev = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.dev.connect((self._host, self._port))
+                self.connected = True
+                time.sleep(1)
 
-        return 0
+            except:
+                import traceback
+                traceback.print_exc()
+                self.connected = False
+                self.dev = None
 
     def on_deactivate(self):
         """ Deinitialisation performed during deactivation of the module.
         """
-        self._device.close()
+        if self.connected:
+            self.dev.close()
         return 0
 
-    def query(self, axis_label, command):
-        """
+    def send(self, command):
+        cmd = command.rstrip()+"\r\n"
+        self.dev.send(cmd.encode())
 
-        :param axis_label:
-        :param command:
-        :return:
-        """
-        return self._device.query("{}?".format(command)).split(command)[1]
+    def read_scpi(self):
+        if self.receive(1)[0] != ord("#"):
+            return bytearray([])
+        else:
+            header_len = int(self.receive(1).decode())
+            if header_len < 0:
+                return bytearray([])
+            else:
+                data_len = int(self.receive(header_len).decode())
+                if data_len <= 0:
+                    return bytearray([])
 
-    def write(self, command, value):
-        """
+                else:
+                    return self.receive(data_len)
 
-        :param axis_label:
-        :param command:
-        :return:
-        """
-        self._device.write("{}={}".format(command, value))
+    def receive(self, length=-1):
+        data_read = length
+        answer = bytearray([])
+        buffer = bytearray([])
+        if not isinstance(length, int):
+            raise Exception('[Receive] Data length must be passed as integer')
+
+        if not self.connected:
+            raise Exception('[Receive] Error. Not connected')
+        else:
+            try:
+                if length == 0:
+                    answer = bytearray([])
+
+                elif length > 0:
+                    while data_read > 0:
+                        buffer = self.dev.recv(data_read)
+                        answer.extend(buffer)
+                        data_read -= len(buffer)
+                else:
+                    while True:
+                        buffer = self.dev.recv(1)
+                        if buffer[0] != 0:
+                            answer.extend(buffer)
+                        if buffer[0] == 0x0a:
+                            break
+            except:
+                import traceback
+                traceback.print_exc()
+                raise Exception('[Receive] Error while reading data')
+
+            return answer
+
+    def query(self, command, block=False):
+        answer = bytearray([])
+        self.send(command)
+        if block == False:
+            answer = self.receive().decode().rstrip()
+        else:
+            answer = self.read_scpi()
+
+        return answer
+
+    def idn(self):
+        return self.query("*idn?")
+
+    def stb(self):
+        return self.query("*stb?")
+
+    def oper(self):
+        return self.query("*oper?")
 
     def get_constraints(self):
         """ Retrieve the hardware constrains from the motor device.
@@ -184,10 +250,21 @@ class Harmonixx(Base, MotorInterface):
         """
         pos_dict = {}
         for label, rel_pos in param_dict.items():
-            axis_pos = float(self.query(label))
-            self.write(label, axis_pos+rel_pos)
-            pos_dict[label] = float(self.query(label))
-
+            if label == "WAVELENGTH":
+                cmd = self._cmd_from_axis[label]
+                abs_pos = self._axis_pos[label] + rel_pos
+                wl = str(int(abs_pos*1e9))
+                wl = (4-len(wl))*'0'+wl if len(wl)!= 4 else wl
+                self._device.write("{}{}".format(cmd, wl))
+                self._device.read()
+                time.sleep(0.2*abs(abs_pos*1e9-self._axis_pos["WAVELENGTH"]*1e9))
+                self._device.read()
+                pos_dict[label] = float(self._device.read()[3:])*1e-9
+            else:
+                cmd = self._cmd_from_axis[label]
+                self._device.write("{}{}".format(cmd, int(rel_pos)))
+                self._device.read()
+                pos_dict[label] = float(self._device.read()[3:].split(" ")[self._axis_index[label]])
         return pos_dict
 
     def move_abs(self, param_dict):
@@ -199,9 +276,22 @@ class Harmonixx(Base, MotorInterface):
         """
         pos_dict = {}
         for label, abs_pos in param_dict.items():
-            self.write(label, abs_pos)
-            pos_dict[label] = float(self.query(label))
-
+            if label == "WAVELENGTH":
+                cmd = self._cmd_from_axis[label]
+                wl = str(int(abs_pos*1e9))
+                wl = (4-len(wl))*'0'+wl if len(wl) != 4 else wl
+                self._device.write("{}{}".format(cmd, wl))
+                self._device.read()
+                time.sleep(0.2*abs(abs_pos*1e9-self._axis_pos["WAVELENGTH"]*1e9))
+                self._device.read()
+                pos_dict[label] = float(self._device.read()[3:])*1e-9
+            else:
+                cmd = self._cmd_from_axis[label]
+                rel_pos = str(int(abs(self._axis_pos[label] - abs_pos)))
+                rel_pos = (3-len(rel_pos))*'0'+wl if len(rel_pos) < 3 else rel_pos
+                self._device.write("{}{}".format(cmd, rel_pos))
+                self._device.read()
+                pos_dict[label] = float(self._device.read()[3:].split(" ")[self._axis_index[label]])
         return pos_dict
 
     def abort(self):
@@ -209,6 +299,7 @@ class Harmonixx(Base, MotorInterface):
 
         @return int: error code (0:OK, -1:error)
         """
+        self._device.write("BRK")
         return 0
 
     def get_pos(self, param_list=None):
@@ -222,7 +313,13 @@ class Harmonixx(Base, MotorInterface):
             param_list = [label for label in self._axis]
         pos_dict = {}
         for label in param_list:
-            pos_dict[label] = float(self.query(label))
+            if label == "WAVELENGTH":
+                pos_dict[label] = self._axis_pos[label]
+            else:
+                cmd = self._cmd_from_axis[label]
+                self._device.write("{}+0".format(cmd))
+                self._device.read()
+                pos_dict[label] = float(self._device.read()[3:].split(" ")[self._axis_index[label]])
 
         return pos_dict
 
@@ -239,11 +336,11 @@ class Harmonixx(Base, MotorInterface):
         """
         if not param_list:
             param_list = [label for label in self._axis]
-        pos_dict = {}
+        status_dict = {}
         for label in param_list:
-            pos_dict[label] = True
+            status_dict[label] = True
 
-        return pos_dict
+        return status_dict
 
     def calibrate(self, param_list=None):
         """ Calibrates the rotation motor
@@ -256,8 +353,15 @@ class Harmonixx(Base, MotorInterface):
             param_list = [label for label in self._axis]
         pos_dict = {}
         for label in param_list:
-            self.write(label, 0)
-            pos_dict[label] = float(self.query(label))
+            if label == "WAVELENGTH":
+                self._device.write("{}".format(label))
+                time.sleep(2)
+                self._device.read()
+                pos_dict[label] = float(self._device.read()[3:])*1e-9
+        else:
+                self._device.write("{}+0".format(label))
+                self._device.read()
+                pos_dict[label] = float(self._device.read()[3:].split(" ")[self._axis_index[label]])
 
         return pos_dict
 
